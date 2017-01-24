93d3eab452fa89af964ca9c0a69c0b290eb794ee
Wenqi He <hewenqi96@gmail.com>
2017-01-23 19:29:00 -0500

    Implemented calc in Person5

--- src/main/java/edu/gatech/oad/antlab/person/Person5.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person5.java
@@ -9,16 +9,16 @@ package edu.gatech.oad.antlab.person;
  *  @version 1.1
  */
 public class Person5 {
-  /** Holds the persons real name */
-  private String name;
+  	/** Holds the persons real name */
+	private String name;
   	/**
 	 * The constructor, takes in the persons
 	 * name
 	 * @param pname the person's real name
 	 */
-  public Person5(String pname) {
-    name = pname;
-  }
+	public Person5(String pname) {
+    	name = pname;
+    }
   	/**
 	 * This method should take the string
 	 * input and return its characters rotated
@@ -30,8 +30,8 @@ public class Person5 {
 	 * @return the modified string
 	 */
 	private String calc(String input) {
-	  //Person 5 put your implementation here
-	  return null;
+	//Person 5 put your implementation here
+	  	return input.substring(2) + input.substring(0,2);
 	}
 	
 	/**
@@ -43,7 +43,7 @@ public class Person5 {
 	 *         object
 	 */
 	public String toString(String input) {
-	  return name + calc(input);
+		return name + calc(input);
 	}
 
 }
