cfa6834c002fcd01abdf677c09689b4cba963245
Gunou Park <gunou88@gmail.com>
2017-01-26 20:47:30 -0500

    changed AntLabMain

--- src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -63,10 +63,10 @@ import edu.gatech.oad.antlab.pkg2.*;
 		  //and gburdell with your gt id
 		  Person2 p2 = new Person2("P2");
 		  toPrint += p2.toString("gburdell2");
-		  //Person3 replace P3 with your name
-		  //and gburdell3 with your gt id
-		  Person3 p3 = new Person3("P3");
-		  toPrint += p3.toString("gburdell3");
+      //Person3 replace P3 with your name
+      //and gburdell3 with your gt id
+      Person3 p3 = new Person3("Gunou Park");
+      toPrint += p3.toString("gpark71");
       //Person4 replace P4 with your name
       //and gburdell4 with your gt id
       Person4 p4 = new Person4("Milind Lingineni");
